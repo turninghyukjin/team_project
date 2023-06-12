df2671612a2523f3d06b7101dc020dc4b0d42736
elly77 <goldeun139977@gmail.com>
2023-06-12 14:56:30 +0900

    cart 파트 작업

--- src/main/java/com/project/sul/entity/Cart.java
+++ src/main/java/com/project/sul/entity/Cart.java
@@ -1,5 +1,33 @@
 package com.project.sul.entity;
 
-public class Cart {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+
+public class Cart extends BaseEntity{
+
+    @Id
+    @Column(name =  "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    @OneToOne
+    @JoinColumn(name="member_id")
+    private Member member;
+
+    public static Cart createCart(Member member){
+        Cart cart = new Cart();
+        cart.setMember(member); //주어진 회원을 카트의 소유자로 설정
+        return  cart;
+    }
 
 }
