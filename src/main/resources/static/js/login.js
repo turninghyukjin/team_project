bfe6427f9a628006ad4bc05155473f86bab3efa5
kimkm12 <kgm34872804@gmail.com>
2023-06-21 09:41:06 +0900

    js적용

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -11,3 +11,22 @@ function toggleLoginForm() {
             loginFormWrapper.style.display = 'none';
         }
     }
+
+
+     document.addEventListener('DOMContentLoaded', function() {
+        var passwordToggleIconHide = document.querySelector('.password-toggle-icon-hide');
+        var passwordToggleIconShow = document.querySelector('.password-toggle-icon-show');
+        var passwordInput = document.querySelector('.email_loginForm_password input[type="password"]');
+
+        passwordToggleIconHide.addEventListener('click', function() {
+          passwordInput.setAttribute('type', 'text');
+          passwordToggleIconHide.style.display = 'none';
+          passwordToggleIconShow.style.display = 'inline-block';
+        });
+
+        passwordToggleIconShow.addEventListener('click', function() {
+          passwordInput.setAttribute('type', 'password');
+          passwordToggleIconHide.style.display = 'inline-block';
+          passwordToggleIconShow.style.display = 'none';
+        });
+      });
